20e88784030f6e1717c634fbeeb44110cb2c4e36
Bennyno <ben.fernandez28@gmail.com>
2022-09-13 00:55:34 +0000

    Created Dom Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let tlds = [".com", ".net", ".org"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(pronoun + adj + noun + tld);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
