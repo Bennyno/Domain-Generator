925bae4feb57ccd406713e8b8685b0eca5f994e2
Bennyno <ben.fernandez28@gmail.com>
2022-09-14 23:37:41 +0000

    Added the domains to the page

--- src/app.js
+++ src/app.js
@@ -7,20 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  document.getElementById("domain").innerHTML = domains();
+};
+function domains() {
   let pronouns = ["the", "our"];
   let adjs = ["great", "big"];
   let nouns = ["jogger", "racoon"];
-  let tlds = [".com", ".net", ".org"];
-
+  let tlds = [".com", ".net", ".org", ".gov"];
+  let domainGenerator = "";
   for (let pronoun of pronouns) {
     for (let adj of adjs) {
       for (let noun of nouns) {
         for (let tld of tlds) {
-          console.log(pronoun + adj + noun + tld);
+          domainGenerator += pronoun + adj + noun + tld + "<br>";
         }
       }
     }
+    return domainGenerator;
   }
-
-  console.log("Hello Rigo from the console!");
-};
+}
